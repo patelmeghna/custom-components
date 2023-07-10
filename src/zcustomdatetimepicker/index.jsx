5237412b581f1571226a343190414a476ad01bb0
Ashikasuthar <ashika.suthar@viitor.cloud>
2023-07-10 12:09:42 -1100

    comments add

--- src/zcustomdatetimepicker/index.jsx
+++ src/zcustomdatetimepicker/index.jsx
@@ -12,7 +12,7 @@ function DatePicker() {
         id="date-picker-1"
         className="custom-date-picker"
         // defaultValue={"2022-02-12 12:12:00 To 2022-01-23 12:20:10"}
-        // minDate
+        minDate
         range
         isMinTime
         // isClear
