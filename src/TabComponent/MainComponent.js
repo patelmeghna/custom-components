c0cb8ce3bb0d4c597b9739c78c0dee5afab0f65a
Ashikasuthar <ashika.suthar@viitor.cloud>
2023-07-31 15:59:22 +0530

    hello

--- src/TabComponent/MainComponent.js
+++ src/TabComponent/MainComponent.js
@@ -7,13 +7,24 @@ const MainComponents = () => {
       id: 'tab1',
       label: 'Ashika',
       content: <p> Hello How Are YOu ?</p>,
+      onClick: () => {
+        console.log("it is working")
+      }
     },
     {
       id: 'tab2',
       label: 'Suthar',
-      content: <p> Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello I Am good </p>,
+      content: <p> Hello Hello Hello Hello Hello Hello  Hello Hello Hello Hello Hello Hello Hello I Am good </p>,
+      onClick: () => {
+        console.log("it is working now")
+      }
     },
+
   ];
+
+//   const handleCustomClick = (tabId) => {
+//     console.log(`Custom click on tab with ID: ${tabId}`);
+//   };
 
   return (
     <div>
